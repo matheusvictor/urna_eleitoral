9862f0bc0316cbb8b2418b3fa0853c97b423b6bc
Matheus Victor <matheusvictor.salles@gmail.com>
2022-10-06 18:55:30 -0300

    feature: Adiciona validação de dítigos

---
+++ src/service/ValidadorDigitos.java
@@ -0,0 +1,19 @@
+package service;
+
+public class ValidadorDigitos {
+
+    public static int verificarQuantidadeDigitos(int numero) { // procurar entender o log resolve esse problema
+        numero = Math.abs(numero); // módulo absoluto do número. Evitar negativos.
+        if (numero == 0) return 1;
+        else {
+            return (int) (Math.log10(numero) + 1);
+        }
+    }
+
+    public static int validarNumeroPartido(int numero) {
+        int quantidadeDigitos = verificarQuantidadeDigitos(numero);
+
+        return numero / (int) Math.pow(10, (quantidadeDigitos - 2));
+    }
+
+}
