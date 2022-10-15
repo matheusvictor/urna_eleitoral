0a0c4b0ae95c94c641aa8de85762140adabb975e
Matheus Victor <matheusvictor.salles@gmail.com>
2022-10-14 22:49:43 -0300

    feature: Adiciona métodos acessores na classe Eleitor

--- src/modelos/Eleitor.java
+++ src/modelos/Eleitor.java
@@ -2,7 +2,30 @@ package modelos;
 
 public class Eleitor extends Pessoa {
 
-    public Eleitor(String nome, char sexo, int idade, String nacionalidade) {
-        super(nome, sexo, idade, nacionalidade);
+    private static int contador = 1;
+
+    private int idEleitor;
+    private boolean jaVotou;
+
+    public Eleitor(String nome, char sexo, int idade) {
+        super(nome, sexo, idade);
+
+        this.jaVotou = false;
+        this.idEleitor = contador;
+
+        contador++;
     }
+
+    public static int getContador() {
+        return contador;
+    }
+
+    public int getIdEleitor() {
+        return idEleitor;
+    }
+
+    public boolean isJaVotou() {
+        return jaVotou;
+    }
+
 }
