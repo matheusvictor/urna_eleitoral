9ad7448f9092e1acb3360547c531118d3a452067
Matheus Victor <matheusvictor.salles@gmail.com>
2022-10-23 12:28:46 -0300

    feature: Cria constantes para números dos Partidos

---
+++ src/constantes/NumerosPartidos.java
@@ -0,0 +1,9 @@
+package constantes;
+
+public final class NumerosPartidos {
+
+    public static final int PLUA = 70;
+    public static final int VELHO = 80;
+    public static final int PARTIDO_DOS_DESEMPREGADOS = 31;
+
+}
