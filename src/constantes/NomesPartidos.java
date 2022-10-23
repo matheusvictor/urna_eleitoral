0018bd476ed433f25f4aaaeaeea3509c0daebdf5
Matheus Victor <matheusvictor.salles@gmail.com>
2022-10-23 12:38:05 -0300

    feature: Modifica enum para nomes de Partido

--- src/constantes/NomesPartidos.java
+++ src/constantes/NomesPartidos.java
@@ -1,5 +1,23 @@
 package constantes;
 
 public enum NomesPartidos {
-    TABAJARA
+
+    PARTIDO_VELHO("Partido Velho"),
+    PARTIDO_LEAO_UVA_ANDORINHA("Partido Leão, Uva e Andorinha"),
+    PARTIDO_DOS_DESEMPREGADOS("Partido dos Desempregrados");
+
+    private String nomePartido;
+
+    NomesPartidos(String nomePartido) {
+        this.nomePartido = nomePartido;
+    }
+
+    public String getNomePartido() {
+        return this.nomePartido;
+    }
+
 }
+
+/*
+    https://www.devmedia.com.br/enums-no-java/38764
+ */
