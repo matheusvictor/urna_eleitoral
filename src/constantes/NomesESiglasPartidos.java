abd12c86e2ab258dbe4354db7ffa3b3a7f3bc24c
Matheus Victor <matheusvictor.salles@gmai.com>
2022-10-24 16:34:30 -0300

    fix: Adiciona sigilas dos Partidos

--- src/constantes/NomesESiglasPartidos.java
+++ src/constantes/NomesESiglasPartidos.java
@@ -2,11 +2,11 @@ package constantes;
 
 public enum NomesESiglasPartidos {
 
-    PARTIDO_VELHO("Partido Velho"),
-    PARTIDO_LOUVAR_UNGIR_AGRADECER("Partido Louvar, Ungir e Agradecer"),
-    PARTIDO_DOS_DESEMPREGADOS("Partido dos Desempregados"),
-    PARTIDO_CAPITALISTA_DO_BRASIL("Partido Capitalista do Brasil"),
-    PARTIDO_CONSERVADOR("Partido Conservador");
+    PARTIDO_VELHO("Partido Velho", "PV"),
+    PARTIDO_LOUVAR_UNGIR_AGRADECER("Partido Louvar, Ungir e Agradecer", "PLUA"),
+    PARTIDO_DOS_DESEMPREGADOS("Partido dos Desempregados", "PD"),
+    PARTIDO_CAPITALISTA_DO_BRASIL("Partido Capitalista do Brasil", "PCdoB"),
+    PARTIDO_CONSERVADOR("Partido Conservador", "PC");
 
     private String nomePartido, siglaPartido;
 
@@ -16,8 +16,13 @@ public enum NomesESiglasPartidos {
         this.siglaPartido = siglaPartido;
     }
 
-    public String getNomePartido() {return this.nomePartido;}
-    public String getSiglaPartido() {return  this.siglaPartido;}
+    public String getNomePartido() {
+        return this.nomePartido;
+    }
+
+    public String getSiglaPartido() {
+        return this.siglaPartido;
+    }
 
 }
 
