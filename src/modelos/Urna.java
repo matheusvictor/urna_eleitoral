9c88d56d4949918580c88ba09b710d5ee800112b
Matheus Victor <matheusvictor.salles@gmail.com>
2022-10-25 19:51:26 -0300

    feat: Filtra Candidatos por Cargo
    Chegar necessidade de refatoração e implementação do método encontrarCandidato

--- src/modelos/Urna.java
+++ src/modelos/Urna.java
@@ -1,21 +1,63 @@
 package modelos;
+
+import constantes.CargosCandidatos;
+import excecoes.CandidatoNaoEncontradoException;
+import service.GeradorDeCandidatosEPartidosService;
+
 import java.util.HashSet;
 
 public class Urna {
-	
-    private HashSet<Candidato> presidentes;
-    private HashSet<Candidato> governadores;
-    private HashSet<Candidato> prefeitos;
-    private HashSet<Candidato> senadores;
-    private HashSet<Candidato> deputadosFederais;
-    private HashSet<Candidato> deputadosEstaduais;
-    private HashSet<Candidato> vereadores;
-    
-    
-    public void inserirCandidato(HashSet<Candidato> c) {
-    	
+
+    private HashSet<Candidato> senadores = new HashSet<>();
+    private HashSet<Candidato> presidentes = new HashSet<>();
+    private HashSet<Candidato> governadores = new HashSet<>();
+    private HashSet<Candidato> deputadosFederais = new HashSet<>();
+    private HashSet<Candidato> deputadosEstaduais = new HashSet<>();
+
+    public Urna(GeradorDeCandidatosEPartidosService service) {
+        HashSet<Candidato> candidatos = service.getCandidatos();
+        for (Candidato c : candidatos) {
+            if (c.getCargo().equals(CargosCandidatos.PRESIDENTE)) {
+                this.presidentes.add(c);
+            } else if (c.getCargo().equals(CargosCandidatos.SENADOR)) {
+                this.senadores.add(c);
+            } else if (c.getCargo().equals(CargosCandidatos.GOVERNADOR)) {
+                this.governadores.add(c);
+            } else if (c.getCargo().equals(CargosCandidatos.DEPUTADO_FEDERAL)) {
+                this.deputadosEstaduais.add(c);
+            } else {
+                this.deputadosFederais.add(c);
+            }
+        }
     }
-    
+
+    private void filtrarCandidatosPorCargo(HashSet<Candidato> candidatos) {
+        // TODO: a filtragem realizada no construtor de Urna poderá acontecer aqui
+    }
+
+
+    public Candidato encontrarCandidato(int numeroCandidato) throws CandidatoNaoEncontradoException {
+        // TODO: implementação ainda incompleta
+        try {
+            for (Candidato c : this.presidentes) {
+                if (c.getNumero() == numeroCandidato) {
+                    System.out.println("OK");
+                }
+            }
+        } catch (Exception e) {
+            throw new CandidatoNaoEncontradoException("N achei");
+        }
+        return null;
+    }
+
+    public void imprimirListaCandidatosAPresidente() {
+        if (this.presidentes != null) {
+            for (Candidato c : this.presidentes) {
+                System.out.println(c.getNome() + " -- " + c.getPartido().getNomePartido());
+            }
+        }
+    }
+
     //incluir candidatos:
     //verificar cargo e inserir numa das coleções acima
     
@@ -40,6 +82,4 @@ public class Urna {
 	-deps feds
      
      */
-    
-
 }
