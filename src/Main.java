5187f24c70ee2591e5728b566ff2a37c5445786a
Matheus Victor <matheusvictor.salles@gmail.com>
2022-10-28 22:35:37 -0300

    feat: Add instância de urna e serviço gerador

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,19 @@
-import constantes.Digitos;
-import modelos.Eleitor;
-import service.ValidadorDigitos;
+import modelos.Candidato;
+import modelos.Urna;
+import service.GeradorDeCandidatosEPartidosService;
+
+import java.util.HashSet;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int numeroPartido = 1234567;
+        GeradorDeCandidatosEPartidosService geradorDeCandidatosEPartidosService = GeradorDeCandidatosEPartidosService.getInstance();
+        Urna urnaEleitoral = new Urna(geradorDeCandidatosEPartidosService);
 
-        int quantidadeDigitos = ValidadorDigitos.verificarQuantidadeDigitos(numeroPartido);
-        System.out.println(quantidadeDigitos);
+        HashSet<Candidato> c = geradorDeCandidatosEPartidosService.getCandidatos();
 
-        int numeroPartidoValidado = ValidadorDigitos.validarNumeroPartido(numeroPartido);
-        System.out.println(numeroPartidoValidado);
-
-        int numeroSenador = (numeroPartidoValidado * 10) + 1;
-        System.out.println(numeroSenador);
-
-        int federal = (int) (numeroPartidoValidado * Math.pow(10, Digitos.DEPUTADO_FEDERAL - Digitos.DIGITOS_PARTIDO) + 0);
-        System.out.println(federal);
-
-        int deputadoEstadual =
-                (int) (numeroPartidoValidado * Math.pow(10, Digitos.DEPUTADO_ESTADUAL - Digitos.DIGITOS_PARTIDO) + 0);
-        System.out.println(deputadoEstadual);
-
-        Eleitor e = new Eleitor("Fulano", 'M', 21, "");
-        System.out.println(e.getIdEleitor());
+        geradorDeCandidatosEPartidosService.imprimirInformacoesDosCandidatos();
 
     }
 }
