eff21a910a9a5380d532da97bbeaedc14a26d7dc
Matheus Victor <matheusvictor.salles@gmail.com>
2022-11-02 16:57:21 -0300

    feat: Adiciona implementações de #8

--- src/modelos/Urna.java
+++ src/modelos/Urna.java
@@ -5,6 +5,8 @@ import excecoes.CandidatoNaoEncontradoException;
 import service.GeradorDeCandidatosEPartidosService;
 
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Urna {
 
@@ -14,9 +16,31 @@ public class Urna {
     private HashSet<Candidato> deputadosFederais = new HashSet<>();
     private HashSet<Candidato> deputadosEstaduais = new HashSet<>();
 
+    private int votosNulos;
+    private int votosEmBranco;
+
     public Urna(GeradorDeCandidatosEPartidosService service) {
         HashSet<Candidato> candidatos = service.getCandidatos();
         filtrarCandidatosPorCargo(candidatos);
+
+        this.votosNulos = 0;
+        this.votosEmBranco = 0;
+    }
+
+    public void incrementarVotosNulos() {
+        this.votosNulos++;
+    }
+
+    public int getVotosNulos() {
+        return votosNulos;
+    }
+
+    public void incrementarVotosEmBranco() {
+        this.votosEmBranco++;
+    }
+
+    public int getVotosEmBranco() {
+        return votosEmBranco;
     }
 
     private void filtrarCandidatosPorCargo(HashSet<Candidato> candidatos) {
@@ -59,30 +83,56 @@ public class Urna {
         }
     }
 
-    //incluir candidatos:
-    //verificar cargo e inserir numa das coleções acima
-    
-     
-     /*  
-	VOTO MAJORITÁRIO (aquele que atigir 50% dos votos + 1 leva no primeiro turno)
+    // implementações de Marcos:
 
-	-prefeito
-	-governadores
-	-senadores
-	-presidente
+    public void votar(HashSet<Candidato> cd, int numeroInserido) { //uma rotina anterior devera tratar o numero inserido
 
+        if (numeroInserido == 0) {  //valor escolhido para branco
+            incrementarVotosEmBranco();
+        } else {
+            if (cd != null) {
+                boolean consta = false;
 
-	VOTO PROPORCIONAL (ocupação de cadeiras de acordo com votos)
-	
-	Todos candidatos ingressam em maior ou menor número.
-	*Na prática, independente de o voto ser individual ou de legenda,
-	o voto é contabilizado por partido.
+                for (Candidato i : cd) {
 
-	-vereadores
-	-deps ests
-	-deps feds
-     
-     */
+                    if (i.getNumero() == numeroInserido) { //candidato encontrado, recebe voto
+                        // TODO: incrementar número de votos no candidato
+                        consta = true;
+                        break;
+                    }
+
+                }
+                if (!consta) {
+                    incrementarVotosNulos();  //numero inserido não encontrado
+                }
+            }
+        }
+
+    }
+
+    public LinkedList<Candidato> apurarVotos(HashSet<Candidato> c) {            //recebe uma lista de candidatos de mesmo cargo
+
+        LinkedList<Candidato> ranking = new LinkedList<Candidato>();        // manter ordem na inserção
+
+        if (!c.isEmpty() && c.size() > 1) {                             //se  lista tiver mais de 1 candidato
+
+            while (c.size() > 0) { //enquanto houver candidatos na lista recebida
+
+                Candidato maisVotado = ((List<Candidato>) c).get(0);   //pega primeiro candidato e o assume como mais votado
+
+                for (int i = 1; i < c.size(); i++) {
+
+                    if (((List<Candidato>) c).get(i).getNumeroVotos() > maisVotado.getNumeroVotos()) {  //compara com o proximo
+
+                        maisVotado = ((List<Candidato>) c).get(i);
+                    }
+                }
+                ranking.add(maisVotado);
+                c.remove(maisVotado);
+            }
+        }
+        return ranking;
+    }
 
 
 }
