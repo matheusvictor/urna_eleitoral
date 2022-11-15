62c03957c3e1794bdd0c17a0fa04765431985045
Matheus Victor <matheusvictor.salles@gmail.com>
2022-11-15 17:30:37 -0300

    refactor: Altera mensagem

--- src/excecoes/CandidatoNaoEncontradoException.java
+++ src/excecoes/CandidatoNaoEncontradoException.java
@@ -3,7 +3,7 @@ package excecoes;
 public class CandidatoNaoEncontradoException extends Exception {
 
     public CandidatoNaoEncontradoException() {
-        super("Candidato não encontrado!");
+        super("Candidato não encontrado! Voto será contabilizado como nulo.");
     }
 
 }
