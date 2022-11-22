7874287d67414cdbd50e4de6b44392b9da09d110
Matheus Victor <matheusvictor.salles@gmail.com>
2022-11-22 08:58:17 -0300

    feat: Adiciona exceção

---
+++ src/excecoes/DigitosExcedenteException.java
@@ -0,0 +1,9 @@
+package excecoes;
+
+public class DigitosExcedenteException extends Exception {
+
+    public DigitosExcedenteException() {
+        super("Número de dígitos excedente!");
+    }
+
+}
